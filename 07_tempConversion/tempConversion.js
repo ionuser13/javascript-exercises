b78f4728e898e805984b70a118250b65ca9346d7
userjohn <johnchacpi479@gmail.com>
2022-09-26 13:42:58 -0500

    seventh exercise done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let cTemp = (tempF - 32)*(5/9)
+  return Math.round(cTemp * 10)/10
 };
 
-const ctof = function() {
-
-};
+const ctof = function(tempC) {
+  let fTemp = tempC * (9/5) + 32;
+  return Math.round(fTemp*10)/10
+};  
 
 // Do not edit below this line
 module.exports = {
