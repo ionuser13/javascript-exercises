53452ffa147cfb2272c8f1df7ddb141923bf37f6
userjohn <johnchacpi479@gmail.com>
2022-09-26 13:06:07 -0500

    fifth exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let min;
+    let max;
+    if((int1 > 0 && int2 > 0)&&(typeof int1 === "number" && typeof int2 === "number")){
+        if (int1>int2){
+            min = int2;
+            max = int1;
+        }
+        else {
+            min = int1;
+            max = int2;
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    return ((max-min)+1)*(min + max)/2;
 };
 
 // Do not edit below this line
