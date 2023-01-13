d002c05cc660739229b0b6d0f3e5b64558cd3c84
John Chacpi <johnchacpi479@gmail.com>
2023-01-12 19:54:55 -0500

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    let n = parseInt(num);
+    if(n>0){let a = 0, b = 1; c = n;
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c}
+    else {
+        return "OOPS"
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
