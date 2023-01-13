49371e983f20aa38e6d5c24473ee5da5ee029c56
John Chacpi <johnchacpi479@gmail.com>
2023-01-12 19:01:57 -0500

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, currentNum) => total + currentNum, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.length ? arr.reduce((total, nextNum) => total * nextNum) : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  if(a>0) {
+    for (let i = a; i>0; i--) {
+      result = result*i;
+    }
+    return result;
+  }
+  else if (a === 0) {
+    return result;
+  }
 };
 
 // Do not edit below this line
