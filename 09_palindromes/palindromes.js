0f02dde732db6c9b115befbe0957ff31ace01718
John Chacpi <johnchacpi479@gmail.com>
2023-01-12 19:17:05 -0500

    solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let regEx = /[^a-z]/g;
+    let modStr = str
+        .toLowerCase()
+        .replace(regEx, "");
+    let reverseStr = modStr
+        .split("")
+        .reverse()
+        .join("");
+    return reverseStr == modStr
 };
 
 // Do not edit below this line
